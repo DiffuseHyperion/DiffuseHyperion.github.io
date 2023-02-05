d251e1cbb1eb6e1680e9b700cd0ed0275e82c6ad
DiffuseHyperion <xieyingjie08@gmail.com>
2023-02-05 12:45:14 +0800

    add languages and started on projects

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 var controller = new ScrollMagic.Controller();
 
-		// define movement of panels
 var wipeAnimation = new TimelineMax()
+	// animate intro
+	.fromTo("#panel1 h1", {y: "-20%", opacity: "0"}, {y: "0", opacity: "1", duration: "1"}, 0)
+	.fromTo("#panel1 h2", {y: "-20%", opacity: "0"}, {y: "0", opacity: "1", duration: "1"}, 0.25)
+	.fromTo("#panel1 h3", {y: "-20%", opacity: "0"}, {y: "0", opacity: "1", duration: "1"}, 0.50)
+	.fromTo("#panel1 h4", {y: "-20%", opacity: "0"}, {y: "0", opacity: "1", duration: "1"}, 0.75)
+
 	// animate to second panel
-	.to("#panel-container", 1, {x: "-25%"})
+	.to("#panel-container", {x: "-25%", duration: "0.5"}, 1.5)
+
+	// animate java language
+	.fromTo("#language1", {x: "-100%", scale: "0", opacity: "0"}, {opacity: "100", x: "0", scale: "1.5", duration: "0.5"}, 2)
+	.to("#language1", {opacity: "0", x: "100%", scale: "0", duration: "0.5"}, 2.5)
+	
+	// animate python language
+	.fromTo("#language2", {x: "-100%", scale: "0", opacity: "0"}, {opacity: "100", x: "0", scale: "1.5", duration: "0.5"}, 2.5)
+	.to("#language2", {opacity: "0", x: "100%", scale: "0", duration: "0.5"}, 3)
+	
 	// animate to third panel
-	.to("#panel-container", 1, {x: "-50%", delay: 2})
+	.to("#panel-container", {x: "-50%", duration: "0.5"}, 3.5)
+
 	// animate to forth panel
-	.to("#panel-container", 1, {x: "-75%" , delay: 2});
+	.to("#panel-container", {x: "-75%"}, 5);
 
 new ScrollMagic.Scene({
 	triggerElement: "#pin-container",
