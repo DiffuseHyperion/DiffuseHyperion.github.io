7c0623953643fb0ef9b68fcf770b6d411e3910d6
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2023-04-07 14:45:31 +0000

    added cards to each prompts finished up projects (except pictures)

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var controller = new ScrollMagic.Controller();
 
 var wipeAnimation = new TimelineMax()
 	// animate hint to scroll down
-	.fromTo("#panel1 h4", {opacity: "1"}, {opacity: "0", duration: "0.1"}, 0)
-
+	.fromTo("#panel1 #hint", {opacity: "1"}, {opacity: "0", duration: "0.1"}, 0)
+	.fromTo("#panel1 .card", {opacity: "0"}, {opacity: "1", duration: "0.25"}, 0)
 	// animate intro
 	.fromTo("#panel1 h1", {y: "-20%", opacity: "0"}, {y: "0", opacity: "1", duration: "1"}, 0)
 	.fromTo("#panel1 h2", {y: "-20%", opacity: "0"}, {y: "0", opacity: "1", duration: "1"}, 0.25)
